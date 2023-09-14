09470e882a3440a6e896fe1a86b25884ca45bb7e
Joshua Müller <josh2197@gmail.com>
2023-09-14 12:41:13 +0200

    changed case handling to be more stable; added metadata to file;

--- process_msems.py
+++ process_msems.py
@@ -1,56 +1,112 @@
+### Here should be some documentation
+
+
+
+
 import pandas as pd
 import os
 import numpy as np
 import xarray as xr
 import argparse
 
+def check_file_type(file_name):
+    """
+    Checks if the file is a raw or processed file
+    """
 
-def read_data(file_name, raw):
-    # Skip the initial configuration lines
-    if raw:
-        data = pd.read_csv(file_name, skiprows=55, delimiter='\t')
+    # Mapping of keywords/phrases to their corresponding file types
+    keyword_mapping = {
+        '#UAV Reader Version': 'uav',
+        '#Date': 'igor',
+        '#YY/MM/DD': 'raw'
+    }
 
-        raw_bin_cols = [col for col in data.columns if 'bin' in col]
+    file_type = []
 
-        dia = np.arange(len(raw_bin_cols))
-        conc = data[raw_bin_cols]
+    type_property_dict = { 
+        'igor': {'columns': ['Bin_Dia', 'Bin_Conc'],
+                 'skiprows': 56,
+                 'delimiter': '\t',
+                 'date_col' : '#Date',
+                 'time_col' : 'Time'
+        },
+        'uav': {'columns': ['bin_dia', 'bin_conc'],
+                'skiprows': 56,
+                'delimiter': '\t',
+                'date_col' : '#YY/MM/DD',
+                'time_col' : 'HR:MN:SC'
+        },
+        'raw': {'columns': ['bin'],
+                'skiprows': 55,
+                'delimiter': '\t',
+                'date_col' : '#YY/MM/DD',
+                'time_col' : 'HR:MN:SC'
+        },
+    }
 
-    else:        
-        data = pd.read_csv(file_name, skiprows=56, delimiter='\t')
+    
+    metadata_dict = {}
 
-        bin_dia_cols = [col for col in data.columns if 'bin_dia' in col]
-        bin_conc_cols = [col for col in data.columns if 'bin_conc' in col]
-        if len(bin_dia_cols) == 0:
-            bin_dia_cols = [col for col in data.columns if 'Dia' in col]
-            bin_conc_cols = [col for col in data.columns if 'Conc' in col]
+    with open(file_name, 'r') as f:
+        for line in f:
+            # Only check lines that start with #
+            if line.startswith("#"):
+                if ':' in line and 'scan_direction' not in line:
+                    key, value = line.split(':')
+                    key = key.strip('#').strip()
+                    value = value.strip('\n')
+                    metadata_dict[key] = value
 
-        dia = data[bin_dia_cols]
-        conc = data[bin_conc_cols]
+                for keyword, ftype in keyword_mapping.items():
+                    if keyword in line:
+                        file_type.append(ftype)
+    
 
+    ### JM Make sure that only one file type was found; the first one is the right one
+    file_type = file_type[0]
+
+
+    return file_type, metadata_dict, type_property_dict[file_type]
+
+
+
+def read_data(file_name, file_type, type_property_dict,):
+
+    ### JM type definitons missing
+
+    data = pd.read_csv(file_name, skiprows=type_property_dict['skiprows'], delimiter=type_property_dict['delimiter'])
+
+
+    ### JM Handling the raw-data case explictly, as there is only one bin there...
+
+    if len(type_property_dict['columns']) == 1:
+
+        conc = data[[col for col in data.columns if type_property_dict['columns'][0] in col]]
+        dia  = np.arange(1, conc.shape[1]+1)
+
+    else:
+
+
+
+        dia = data[[col for col in data.columns if  type_property_dict['columns'][0] in col]]
+        conc = data[[col for col in data.columns if type_property_dict['columns'][1] in col]]
 
     num_rows = data.shape[0]
-    print('Number of rows: ', num_rows)
-
-
-
-    try: 
-        time = data['#YY/MM/DD']+'T'+data['HR:MN:SC']
-        time = pd.to_datetime(time, format='%y/%m/%dT%H:%M:%S')
-
-    except KeyError:
-        time = data['#Date']+'T'+data['Time']        
-        time = pd.to_datetime(time, format='%y/%m/%dT%H:%M:%S')
+    print('Number of Scans: ', num_rows)
+    
+    time = data[type_property_dict['date_col']]+'T'+data[type_property_dict['time_col']]
+    time = pd.to_datetime(time, format='%y/%m/%dT%H:%M:%S')
 
     return dia, conc, time
 
 
-def data_to_netcdf(data, output_dir,raw=False):
+def data_to_netcdf(data, output_dir, filename, metadata_dict=None, file_type=None):
 
-    bin, conc, time = read_data(data, raw)
+    bin, conc, time = data
     bin = np.array(bin, dtype=np.float32)
     conc = np.array(conc, dtype=np.float32)
 
-    name = data.split('/')[-1].split('.')[0]
+    name = filename.split('/')[-1].split('.')[0]
 
 
     ### JM this should still be changed; right now the average bin is taken and not interpolated to the same bin size
@@ -63,36 +119,45 @@ def data_to_netcdf(data, output_dir,raw=False):
 
     xr_data = xr.DataArray(conc, dims=['time', 'bin'], coords={'time': time, 'bin': bin_mean})
 
-    xr_data = xr_data.to_dataset(name='conc').to_netcdf(f'{output_dir}{name}.nc')
 
-    ### JM still the metadata is missing; this should be added
-    ### time, place, sheath air blabla
+    ### JM add metadata to the netcdf file
 
+    xr_data.attrs = metadata_dict
+    xr_data.attrs['file_type'] = file_type
 
-    if raw:
-        xr_data.attrs['raw'] = True
-    else:
-        xr_data.attrs['raw'] = False
-
-
+    ### JM save the netcdf file
+    
+    xr_data.to_dataset(name='conc').to_netcdf(f'{output_dir}{name}.nc')
 
 
     return xr_data
 
 
+
 if __name__ == '__main__':
 
 
-    parser = argparse.ArgumentParser(description='Converts a raw or processed mSEMS data file to netcdf')
+    parser = argparse.ArgumentParser(description='Converts raw, igor-inverted or uav-reader-inverted mSEMS data file to netcdf')
 
     parser.add_argument('--file', type=str, help='Path to the data file')
-    parser.add_argument('--raw', help='If the data is raw or processed')
     parser.add_argument('--output_dir', type=str, help='Path to the output directory')
 
     args = parser.parse_args()
 
-    data_to_netcdf(args.file, args.output_dir, args.raw)
+    if args.output_dir is None:
+        args.output_dir = os.getcwd()        
 
+    if args.output_dir[-1] != '/':
+        args.output_dir += '/'
 
+    
+    file_type, metadata_dict, type_property_dict = check_file_type(args.file)
+    print('File type: ', file_type)
+
+    data = read_data(args.file, file_type, type_property_dict)
+
+    data_to_netcdf(data, args.output_dir, args.file, metadata_dict=metadata_dict, file_type=file_type)
+
+    print('Done!')
 
 
